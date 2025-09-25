fd3d93f04e9bf4e5d0293ebd296537b1a0f12ea0
Petrel001 <mxx_mxx2025@163.com>
2025-09-25 23:01:52 +0800

    Test2 updated

--- test2.cpp
+++ test2.cpp
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <ctype.h>
-
+#include <iostream>
+using namespace std;
 int main(){
-    FILE *file;
-    file = fopen("test.text","r");
-    if (file == NULL){
-        perror("Error");
-        return 1;
+    // FILE *file;
+    // file = fopen("test.txt","r");
+    // if (file == NULL){
+    //     perror("Error");
+    //     return 1;
+    // }
+    // scanf("%c",&a);
+    // b = a;
+    // cout<<b<<endl;
+    // // printf("%c",a[1][1]);
+    char *a[101];
+    for (int i = 0;i<2;i++){
+        cin>>a[i];
+    }
+    // cin>>a;
+    // scanf("%s",&a);
+    for (int i = 0;i<2;i++){
+        printf("%s\n",a[i]);
     }
     return 0;
 }
